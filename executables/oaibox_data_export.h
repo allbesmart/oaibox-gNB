2b3e7a8269adfffcf4fed8e3fc036ec88f3db75a
luis_pereira87 <lpereira@allbesmart.pt>
2023-04-21 10:44:43 +0100

    OAIBOX specific features and optimizations

---
+++ executables/oaibox_data_export.h
@@ -0,0 +1,10 @@
+//
+// Created by user on 14/10/21.
+//
+
+#ifndef OPENAIRINTERFACE_OAIBOX_DATA_EXPORT_H
+#define OPENAIRINTERFACE_OAIBOX_DATA_EXPORT_H
+
+void *oaibox_data_export_func();
+
+#endif //OPENAIRINTERFACE_OAIBOX_DATA_EXPORT_H
